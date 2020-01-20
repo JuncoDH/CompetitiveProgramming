8b154047728a03225765f7d9cd0cd58510fa3951
JuncoDH <juncodelasheras@outlook.com>
2020-01-20 10:15:10 +0100

    +k bits on

--- Implementations/Bitmask/k_bits_on.cpp
+++ Implementations/Bitmask/k_bits_on.cpp
@@ -1,3 +1,20 @@
+//Digit DP, number of numbers lower or equal than num with cnt bits on
+//sweep from left to right with the position pos. cnt is the number of bits to set on and 
+//f1 is a flag whether your current number is lower than num (You construct the number but don't keep the number).
+//With a modification you can do it for decimal system
+ll dp[64][2][5];
+ll num;
+ll f(ll pos, ll f1, ll cnt) {
+    if(cnt == 0) return 1;
+    if(cnt > pos+1) return 0;
+    if(pos < 0) return 0;
+    if(dp[pos][f1][cnt] != -1) return dp[pos][f1][cnt];
+    
+    if(f1 == 1 || ((1ll<<pos) & num) != 0) return dp[pos][f1][cnt] = f(pos - 1, f1, cnt-1) + f(pos - 1, 1, cnt);
+    else return dp[pos][f1][cnt] = f(pos - 1, f1, cnt);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
 map<pll, ll> m;
 
 //num of numbers lower or equal than x with k bits set on
