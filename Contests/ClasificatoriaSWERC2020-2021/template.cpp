b59c60dc49f5057c1bc37e2e8511495b180d28fd
JuncoDH <juncodelasheras@outlook.com>
2020-12-17 12:16:43 +0100

    clasificatoria swerc 2020-2021

---
+++ Contests/ClasificatoriaSWERC2020-2021/template.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+
+using ll=long long;
+using pii=pair<int,int>;
+using vi=vector<int>;
+
+
+int main(){
+    cout<<"Hi I do\n";
+    return 0;
+}
---
+++ Online_Judges/UVA/10924 - Prime Words.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define LSB(x) ((x) & (-(x)))
+#define is_set(x, i) (((x)>>(i))&1)
+#define set_bit(x, i) {(x) |= 1ll<<(i);}
+
+#ifdef JUNCO_DEBUG
+#define echoarr(x) {for(int _i=0,_n=min((size_t)15,sizeof(x)/sizeof(x[0]));_i<_n;_i++)\
+ {cout << x[_i] << " ";} cout << endl;}
+#define echo(...) {cout<<"->";ECHO(#__VA_ARGS__, __VA_ARGS__ );cout<<endl;}
+#define echo2(x) {cout<<#x<<endl; for(auto el : x) {ECHO(el); cout << endl;}}
+#else
+#define echoarr(x)
+#define echo(...) 
+#define echo2(x) 
+#endif
+
+const long double PI = acos(-1);
+const long double eps = 1e-9;
+const long long inf = LLONG_MAX / 10;
+
+void ECHO(string _s){cout<<_s;}
+void ECHO(bool _s){if(_s)cout<<"true";else cout<<"false";}
+void ECHO(char _s){cout<<_s;}
+void ECHO(long unsigned _s) {cout<<_s;}//for s.size()
+void ECHO(int _s){cout<<_s;}
+void ECHO(long long _s){if(_s == inf)cout << "inf";else cout<<_s;}
+void ECHO(double _s){cout<<_s;}
+void ECHO(long double _s){cout<<_s;}
+void ECHO(bitset<8> b) {cout << b;}//resize bitset
+template<typename T> void ECHO(set<T> _s) {
+    for(auto el : _s) {ECHO(el); cout << "_";}
+}
+template<typename T1, typename T2> void ECHO(map<T1, T2> _s) {
+    for(auto el : _s) {ECHO(el); cout << "_";}
+}
+template<typename T1, typename T2> void ECHO(pair<T1, T2> _s) {
+    cout << "("; ECHO(_s.fi); cout << ","; ECHO(_s.se); cout << ")";
+}
+template<typename T> void ECHO(vector<T> v) {
+    for(auto el : v) {ECHO(el); cout << "_";}
+}
+template<typename T, typename ...Args> void ECHO(string _s, T x, Args... args){
+    int _i, c = 0;
+    for(_i = 0; _i < (int)_s.size(); _i++) {
+        if(c==0 && _s[_i]==',') break;
+        if(_s[_i] == '(') c++;
+        if(_s[_i] == ')') c--;
+    }
+    cout << " (" << _s.substr(0, _i) << "): "; ECHO(x);
+    while(_i < (int)_s.size() && (_s[_i] == ',' || _s[_i] == ' ')) _i++;
+    ECHO(_s.substr(_i), args...);
+}
+
+template<typename T> // Input vector<T>.
+istream& operator >> (istream& is, vector<T>& v) {
+    for(T& el : v) is >> el;
+    return is;
+}
+
+template<typename T> // Output vector<T>.
+ostream& operator << (ostream& os, const vector<T>& v) {
+    bool _first = false;
+    for(T el : v) {
+        if(_first) os << " ";
+        _first = true;
+        os << el;
+    }
+    return os;
+}
+
+typedef long double ld;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    string s;
+    ll sum, sq, i;
+    bool ok;
+    while(getline(cin, s)) {
+        sum = 0;
+        for(auto c : s) {
+            if('a' <= c && c <= 'z') sum += c-'a'+1;
+            else if('A' <= c && c <= 'Z') sum += c-'A'+27;
+        }
+        sq = sqrt(sum) + 3;
+        sq = min(sq, sum-1);
+        ok = true;
+        for(i = 2; i <= sq; i++) {
+            if(sum%i == 0) {ok = false; break;} 
+        }
+        // Here 1 is considered prime ...
+        if(ok) cout << "It is a prime word.\n";
+        else cout << "It is not a prime word.\n";
+    }
+
+
+
+    return 0;
+}
