c71bc81933ecebdc1e39b8e5423f0450f27e72f2
JuncoDH <juncodelasheras@outlook.com>
2021-02-23 16:21:27 +0100

    inverse 1 to n

---
+++ Implementations/Math/gcd/inverse_1_N.cpp
@@ -0,0 +1,10 @@
+const ll mod = 31;
+ll inverse[mod]; 
+// O(mod) calculate inverse[i] % const mod.
+void calc() {
+    inverse[1] = 1;
+    for(ll i = 2; i < mod; i++) {
+        inverse[i] = -(mod/i)*inverse[mod%i];
+        inverse[i] = (inverse[i]%mod + mod) % mod;
+    }
+}
