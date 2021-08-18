7bbac4d5cdd4bcba44f8f9a5e9431be3c10ca429
JuncoDH <juncodelasheras@outlook.com>
2021-08-18 20:29:08 +0200

    + comments

--- Implementations/DataStructure/PersistentSegmentTree.cpp
+++ Implementations/DataStructure/PersistentSegmentTree.cpp
@@ -1,24 +1,24 @@
-const int MAX_VERSION = 3*1e4+4; //maximum number of versions
+const int MAX_VERSION = 3*1e4+4; // Maximum number of versions.
 template<typename T>
 struct node {
     node *pl = NULL, *pr = NULL;
     int l, r, mid;
-    T value = 0; //Sum segTree
+    T value = 0; // Sum query.
     node(int _l, int _r) {l = _l; r = _r; mid = (l+r)>>1;}
     node(int _l, int _r, T _value) {l = _l; r = _r; value = _value; mid = (l+r)>>1;}
-    void update() { //Sum segTree
+    void update() { // Sum query.
         value = 0;
         if(pl) value += pl->value;
         if(pr) value += pr->value;
     }
-}; // Declare outside, else static memory gives seg fault
-node<ll> *root[MAX_VERSION]; //it stores the i versions after updates
+}; // Declare outside, else static memory gives seg fault.
+node<ll> *root[MAX_VERSION]; //it stores the i versions after updates, start at 0.
 
 template<typename T>
 class PersistentSegmentTree {
-    vector<T> arr; //copy of the array to build SegmentTree
+    vector<T> arr; // Copy of the array to build SegmentTree.
 
-    void build(node<T> *n) { //O(n)
+    void build(node<T> *n) { // O(n).
         if(n->l == n->r) {n->value = arr[n->l]; return;}
         n->pl = new node<T>(n->l, n->mid);
         n->pr = new node<T>(n->mid+1, n->r);
@@ -26,7 +26,7 @@ class PersistentSegmentTree {
         build(n->pr);
         n->update();
     }
-    node<T>* update(node<T> *n, int q, ll x) { //O(logn)
+    node<T>* update(node<T> *n, int q, ll x) { // O(logn).
         if(n->l == n->r) {
             return new node<T>(n->l, n->r, x);
         }
@@ -41,48 +41,48 @@ class PersistentSegmentTree {
         nod->update();
         return nod;
     }
-    T query(node<T> *n, int l, int r) { //O(logn)
+    T query(node<T> *n, int l, int r) { // O(logn).
         if(l <= n->l && n->r <= r) return n->value;
-        if(r < n->l || n->r < l) return 0; //Sum segTree
+        if(r < n->l || n->r < l) return 0; // Sum query.
         return query(n->pl, l, r) + query(n->pr, l, r);
     }
     public:
     PersistentSegmentTree() = default;
-    PersistentSegmentTree(int n){ //Build from empty vector of size n
+    PersistentSegmentTree(int n){ // Build from empty vector of size n.
         arr.assign(n, 0);
         root[0] = new node<T>(0, n-1);
         build(root[0]);
     }
-    PersistentSegmentTree(vector<T> &v) { //Build from vector v
+    PersistentSegmentTree(vector<T> &v) { // Build from vector v.
         arr = v;
         root[0] = new node<T>(0, (int)arr.size() - 1);
         build(root[0]);
     }
-    T query(int version, int l, int r) {return query(root[version], l, r);} //O(logn)
-    // Set v[idx] = x.
-    void update(int version, int new_version, int idx, T x) { //update the segTree version into new_version root
-        root[new_version] = update(root[version], idx, x); //O(logn)
+    T query(int version, int l, int r) {return query(root[version], l, r);} // O(logn).
+    // Set v[idx] = x. Set update.
+    void update(int version, int new_version, int idx, T x) { // update the segTree version into new_version root.
+        root[new_version] = update(root[version], idx, x); // O(logn).
     }
 };
 
 template<typename T>
-class NumberDistinctNumbers { //Works for queries online. For offline can check MO's
+class NumberDistinctNumbers { // Works for queries online. For offline can check MO's.
     PersistentSegmentTree<T> pst;
-    static const int MAX_ELEMENT_VALUE = 1e6+4; // for querying last[el]
+    static const int MAX_ELEMENT_VALUE = 1e6+4; // for querying last[el].
 
     public:
-    NumberDistinctNumbers(vector<T> &v) { //O(nlogn)
-        T last[MAX_ELEMENT_VALUE]; //last ocurrence of the i-number in the array, updating from left to right
+    NumberDistinctNumbers(vector<T> &v) { // O(nlogn).
+        T last[MAX_ELEMENT_VALUE]; // last ocurrence of the i-number in the array, updating from left to right.
         int n = v.size();
         fill(last, last+MAX_ELEMENT_VALUE, -1);
         pst = PersistentSegmentTree<T>(n);
         for(int r = 0; r < n; r++) {
-            pst.update(max(0, r-1), r, r, 1); //Actualize r
-            if(last[v[r]] != -1) pst.update(r, r, last[v[r]], 0); //Remove last[v[r]]
-            last[v[r]] = r; //Actualize last[v[r]]
+            pst.update(max(0, r-1), r, r, 1); // Actualize r.
+            if(last[v[r]] != -1) pst.update(r, r, last[v[r]], 0); // Remove last[v[r]].
+            last[v[r]] = r; // Actualize last[v[r]].
         }
     }
-    T query(int l, int r) { //Return the number of Distinct numbers in [l..r], O(logn)
+    T query(int l, int r) { // Return the number of Distinct numbers in [l..r], O(logn).
         return pst.query(r, l, r);
     }
 };
--- Implementations/String/Hash/Hash_str.cpp
+++ Implementations/String/Hash/Hash_str.cpp
@@ -1,14 +1,14 @@
-//https://www.browserling.com/tools/prime-numbers
-//s = a[i], hash = a[0] + b*a[1] + b^2*a[2] + b^n*a[n]
+// https://www.browserling.com/tools/prime-numbers.
+// s = a[i], hash = a[0] + b*a[1] + b^2*a[2] + b^n*a[n].
 class HashStr {
     public:
     string s;
     int n, n_p;
-    vector<vll> v; //contain the hash for [0..i]
-    vll p = {16532849, 91638611, 83157709}; //prime numbers//15635513  77781229
-    vll base = {37, 47, 53}; //base numbers: primes that > alphabet size //49 83
-    vector<vll> b; //b[i][j] = (b_i^j) % p_i 
-    vector<vll> b_inv;//b_inv[i][j] = (b_i^j)^-1 % p_i 
+    vector<vll> v; // contain the hash for [0..i].
+    vll p = {16532849, 91638611, 83157709}; // prime numbers. // 15635513  77781229
+    vll base = {37, 47, 53}; // base numbers: primes that > alphabet size. // 49 83
+    vector<vll> b; // b[i][j] = (b_i^j) % p_i.
+    vector<vll> b_inv; // b_inv[i][j] = (b_i^j)^-1 % p_i. 
 
     ll elevate(ll a, ll _b, ll mod){
         ll ans = 1;
@@ -19,14 +19,13 @@ class HashStr {
         }
         return ans;
     }
-
-    //a^(mod - 1) = 1, Euler
+    // a^(mod - 1) = 1, Euler.
     ll inv(int i, int j){
         if(b_inv[i][j] != -1) return b_inv[i][j];
         return b_inv[i][j] = elevate(b[i][j], p[i] - 2, p[i]);
     }
-
-    HashStr(string &_s) { //not empty strings
+    HashStr() = default; // Initialize later.
+    HashStr(string &_s) { // not empty strings.
         s = _s;
         n = _s.length();
         n_p = (int)p.size();
@@ -40,7 +39,7 @@ class HashStr {
                 b[i][j] = (b[i][j-1]*base[i]) % p[i];
             }
         }
-        char initial = 'a'; //change initial for range
+        char initial = 'A'; // change initial for range. 'a', 'A'.
         for(i = 0; i < n_p; i++) {
             v[i][0] = s[0]-initial+1;
             for(j = 1; j < n; j++) {
@@ -55,20 +54,20 @@ class HashStr {
         ans = ((ans%p[imod])+p[imod])%p[imod];
         return ans;
     }
-    //O(1)
+    // O(1).
     bool equals(HashStr other, int l, int r) {
         for(int i = 0; i < n_p; i++) {
             if(getHash(l, r, i) != other.getHash(l, r, i)) return false;
         }
         return true;
     }
-    //O(1)
+    // O(1).
     bool operator == (HashStr other) {
         if(n != other.n) return false;
         return equals(other, 0, n-1);
     }
-    //return the index of the Longest Comon Prefix, -1 if no Common Prefix
-    //O(log n)
+    // return the index of the Longest Comon Prefix, -1 if no Common Prefix.
+    // O(log n).
     int LCP(HashStr other) {
         int l = 0, r = min(n, other.n), mid;
         if(s[0] != other.s[0]) return -1;
@@ -85,7 +84,7 @@ class HashStr {
         int id = LCP(other);
         if(id == -1) return s[0] < other.s[0];
         if(*this == other) return false;
-        if(id == n) return true; //"ho" < "hol"
+        if(id == n) return true; // "ho" < "hol"
         if(id == other.n) return false;
 
         return s[id+1] < other.s[id+1];
