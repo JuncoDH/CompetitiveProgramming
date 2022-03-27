f36420b284f3dcd99200588eeb5984af6a798636
JuncoDH <juncodelasheras@outlook.com>
2022-03-27 14:52:38 +0200

    euler tour - hierholzer

---
+++ Implementations/Graph/Euler_tour.cpp
@@ -0,0 +1,18 @@
+// Euler circuit, visit all edges once.
+// Condition: for every node in-degree = out-degree. All edges are in the same SCC (connected).
+// For a Euler path the condition is all nodes in-degree = out-degree, one out-degree+1 = in-degree (start with this node) and one out-degree = in-degree+1.
+vi euler_tour;
+void hierholzer(int u) {
+    int v;
+    while(!graph[u].empty()) {
+        v = graph[u].back(); 
+        graph[u].pop_back(); // DESTROYS THE GRAPH.
+        hierholzer(v);
+    }
+    euler_tour.pb(u);
+}
+
+void f_euler_tour() {
+    hierholzer(0);
+    reverse(euler_tour.begin(), euler_tour.end());
+}
