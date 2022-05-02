76ecc27b7f2eb78d9bff2a8d9d1683d23c0143e3
JuncoDH <juncodelasheras@outlook.com>
2022-05-02 17:25:09 +0200

    li chao tree

---
+++ Implementations/DataStructure/LiChaoTree.cpp
@@ -0,0 +1,56 @@
+// https://robert1003.github.io/2020/02/06/li-chao-segment-tree.html.
+template<typename T>
+class Line{
+    public:
+    T m = 0, n = 0;
+    Line() = default;
+    Line(T _m, T _n) {m = _m; n = _n;}
+    T evaluate(T x) {
+        return m*x + n;
+    }
+};
+// Only insert functions with one intersecction point, like lines.
+// Constraint: query(x) has to be in [0, n-1].
+template<typename T>
+class LiChaoTree{
+    vector<Line<T>> st;
+    int n;
+    // The node will save the best line for the mid point.
+    // Left node is [l, mid] the right node is [mid+1, r].
+    void insert(int k, int l, int r, Line<T> line) {
+        if(l == r) {
+            if(st[k].evaluate(l) < line.evaluate(l)) swap(st[k], line);
+            return;
+        }
+        int mid = (l+r)/2;
+        if(st[k].m < line.m) swap(st[k], line);
+        if(st[k].evaluate(mid) < line.evaluate(mid)) {
+            swap(st[k], line);
+            insert(k<<1|1, mid+1, r, line);
+        } else {
+            insert(k<<1, l, mid, line);
+        }
+    }
+    T query(int k, int l, int r, T x) {
+        T ans = st[k].evaluate(x);
+        if(l == r) {
+            return ans;
+        }
+        int mid = (l+r)/2;
+        if(x <= mid) return max(ans, query(k<<1, l, mid, x));
+        return max(ans, query(k<<1|1, mid+1, r, x));
+    }
+    public:
+    LiChaoTree(int _n) {
+        n = _n;
+        st.assign(4*n, Line<T>());
+    }
+    void insert(Line<T> line) {
+        insert(1, 0, n-1, line);
+    }
+    // Return max evaluated value of all i in a set S = {m_i*x + n_i}.
+    T query(T x) {
+        return query(1, 0, n-1, x);
+    }
+};
+
--- Implementations/ad_hoc/vector_compression.cpp
+++ Implementations/ad_hoc/vector_compression.cpp
@@ -14,3 +14,10 @@ void matrix_compression(vector<vll> &v) {
     for(auto &el : s) m[el] = (int) m.size();
     for(auto &v2 : v) for(auto &el : v2) el = m[el];
 }
+
+void vector_compression_map(vll &v, map<ll, ll> &value2pos, map<ll, ll> &pos2value) {
+    set<ll> s;
+    for(auto el : v) s.insert(el);
+    for(auto el : s) value2pos[el] = value2pos.size();
+    for(auto el : value2pos) pos2value[el.se] = el.fi;
+}
