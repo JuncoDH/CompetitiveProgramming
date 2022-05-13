77959d62494bae7b5d5572d7d9a527497db26c81
JuncoDH <juncodelasheras@outlook.com>
2022-05-13 12:53:19 +0200

    lis

--- Implementations/DP/LIS.cpp
+++ Implementations/DP/LIS.cpp
@@ -1,31 +1,20 @@
-int LIS(vll &v) {
-    int i, n = v.size();
-    vll lis;
-    if(!n) return 0;
-    lis.pb(v[0]);
+vll LIS(vll &v) {
+    int i, t, n = v.size();
+    if(n == 0) return vll();
+    vll lis, lis_t(n), ans;
+    lis.pb(v[0]); lis_t[0] = 1;
     for(i = 1; i < n; i++) {
-        if(v[i] > lis.back()) {lis.pb(v[i]); continue;}
+        // if(v[i] == lis.back()) continue; // For >.
+        if(v[i] >= lis.back())
+            {lis.pb(v[i]); lis_t[i] = lis.size(); continue;}
         int pos = upper_bound(lis.begin(), lis.end(), v[i]) - lis.begin();
-        // if(pos > 0 && lis[pos-1] == v[i]) continue; // USE IF YOU WANT STRICTLY INCREASING.
+        // if(pos > 0 && lis[pos - 1] == v[i]) continue; // For >.
         lis[pos] = v[i];
+        lis_t[i] = pos+1;
     }
-    return lis.size();
-}
-vll v_LIS(vll &v) {
-    int i, j, n = v.size();
-    vll lis, lis_time(n), ans;
-    if(!n) return ans;
-    lis.pb(v[0]); lis_time[0] = 1;
-    for(i = 1; i < n; i++) {
-        if(v[i] > lis.back()) {lis.pb(v[i]); lis_time[i] = lis.size(); continue;}
-        int pos = upper_bound(lis.begin(), lis.end(), v[i]) - lis.begin();
-        // if(pos > 0 && lis[pos-1] == v[i]) continue; // USE IF YOU WANT STRICTLY INCREASING.
-        lis[pos] = v[i];
-        lis_time[i] = pos+1;
-    }
-    j = lis.size();
-    for(i = n-1; i >= 0; i--) {
-        if(lis_time[i] == j && (ans.empty() || v[i] <= ans.back())) {ans.pb(v[i]); j--;} // <= or <.
+    for(i = n-1, t = lis.size(); i >= 0; i--) {
+        if(lis_t[i] == t && (ans.empty() || v[i] <= ans.back()))
+            ans.pb(v[i]), t--; // v[i] < ans.back() for >.
     }
     reverse(ans.begin(), ans.end());
     return ans;
