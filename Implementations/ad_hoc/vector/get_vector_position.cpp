a139667f8431bf49af66578cabbc5681c8458e38
JuncoDH <juncodelasheras@outlook.com>
2022-05-27 18:16:22 +0200

    vector stuff

---
+++ Implementations/ad_hoc/vector/get_vector_position.cpp
@@ -0,0 +1,10 @@
+// vpos[i] is all the positions of element i in the vector.
+vector<vll> get_vector_position(vll &v) {
+    int i, n = v.size();
+    vector<vll> vpos(n+1, vll()); // Elements [0, n].
+    for(i = 0; i < n; i++) {
+        vpos[v[i]].pb(i);
+    }
+    return vpos;
+}
+
