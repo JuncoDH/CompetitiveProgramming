4a76a363f0d66348aade686a00df909daa513b4c
JuncoDH <juncodelasheras@outlook.com>
2022-05-29 10:39:05 +0200

    heap

---
+++ Implementations/DataStructure/Heap.cpp
@@ -0,0 +1,43 @@
+template<typename T> // int, ll, pll.
+class Heap{ // Max Heap, implementation of priority queue.
+    vector<T> v = vector<T>(1, T());
+    int p(int pos) {return pos/2;} // Parent.
+    int cl(int pos) {return 2*pos;} // Left child.
+    int cr(int pos) {return 2*pos+1;} // Right child.
+    public:
+    void push(T el) { // Insert element.
+        int pos = v.size();
+        v.pb(el); // Heapify up.
+        while(pos > 1 && v[pos] > v[p(pos)]) {
+            swap(v[pos], v[p(pos)]);
+            pos = p(pos);
+        }
+    }
+    void pop() {
+        int pos = 1, posc, n = v.size();
+        if(n == 1) return; // Error, empty.
+        swap(v[1], v[n - 1]);
+        v.pop_back();
+        n = v.size();
+        while(true) { // Heapify down.
+            if(cl(pos) >= n) break;
+            posc = cl(pos);
+            if(cr(pos) < n && v[cr(pos)] > v[cl(pos)]) posc = cr(pos);
+            if(v[pos] < v[posc]) {
+                swap(v[pos], v[posc]);
+                pos = posc;
+            } else break;
+        }
+    }
+    T top() {
+        if(v.size() == 1) return numeric_limits<T>::min();
+        return v[1];
+    }
+    int size() {
+        return (int)v.size()-1;
+    }
+    bool empty() {
+        return (int)v.size() == 1;
+    }
+};
+
---
+++ Implementations/ad_hoc/num2binary.cpp
@@ -0,0 +1,9 @@
+// Converts num to binary with n digits.
+string num2binary(ll num, ll digit) {
+    string s = string(digit, '0');
+    for(int i = 0; i < digit; i++) {
+        if(is_set(num, i)) s[digit-i-1] = '1';
+    }
+    return s;
+}
+
